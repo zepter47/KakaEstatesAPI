2d3158613730122d9bcebec471f49975d30b4fc9
Jamil Miyingo <miyingojamil34@gmail.com>
2025-09-11 05:03:15 +0300

    changes introducing different controllers for specific tasks like water and tenant services

---
+++ Respositories/Interfaces/IWaterManager.cs
@@ -0,0 +1,27 @@
+﻿using JamilNativeAPI.Models;
+using System.Collections.ObjectModel;
+
+namespace JamilNativeAPI.Respositories.Interfaces
+{
+    public interface IWaterManager
+    {
+        public Task AddWaterPayment(WaterBillDto bill, string FirstTenant,
+            string LastTenanat, decimal current, decimal previuos, string house, DateTime realTime);
+
+        public Task<ObservableCollection<WaterBillDto>> GetWaterBill(DateTime startDate, DateTime endDate);
+
+        public Task AddWaterAmount(WaterFactorDto amount);
+
+        public Task<decimal> GetWaterAmount(int id);
+
+        public Task UpdateWaterAmount(WaterFactorDto amount, int waterId);
+
+        public Task AddPaymentDetails(decimal amountPaid, int idPayment);
+
+        public Task<ObservableCollection<PaymentDetailsDto>> GetPaymentDetails(int idPayment);
+
+        public Task<decimal> GetPreviousReading(string house);
+
+
+    }
+}
